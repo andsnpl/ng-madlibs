eff3bda7b5816da07059d3c11cee45c3286c0c12
B Paul Anderson <benpaul.anderson@gmail.com>
2015-12-27 22:12:47 -0600

    Gender options

--- src/js/controllers/madlibs.js
+++ src/js/controllers/madlibs.js
@@ -6,7 +6,49 @@ angular.module('ng-madlibs')
   .controller('madlibs', [
     '$scope',
     function ($scope) {
+      $scope.gender = 'F';
+
+      $scope.spaceify = function (snakeCaseName) {
+        return snakeCaseName.replace(/_/g, ' ');
+      };
+
       $scope.ml = function (blankName) {
-        return this[blankName] || '{{' + blankName.replace('_', ' ') + '}}';
+        return this[blankName] || '{{' + this.spaceify(blankName) + '}}';
+      };
+
+      $scope.nameKey = function (gender) {
+        return gender === 'M' ?
+            'male_name' :
+          gender === 'F' ?
+            'female_name' :
+          // gender === 'O' ?
+            'name';
+      };
+
+      $scope.pron_2s_subjective = function (gender) {
+        return gender === 'M' ?
+            'he' :
+          gender === 'F' ?
+            'she' :
+          // gender === 'O' ?
+            'they';
+      };
+
+      $scope.pron_2s_objective = function (gender) {
+        return gender === 'M' ?
+            'him' :
+          gender === 'F' ?
+            'her' :
+          // gender === 'O' ?
+            'them';
+      };
+
+      $scope.pron_2s_genetive = function (gender) {
+        return gender === 'M' ?
+            'his' :
+          gender === 'F' ?
+            'her' :
+          // gender === 'O' ?
+            'their';
       };
     }]);
